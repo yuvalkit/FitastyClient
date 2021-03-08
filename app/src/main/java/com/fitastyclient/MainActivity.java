4f19bd9b230754194056283936fbc6c79f6bb034
Yuval Kittenplon <yuval.kittenplon@live.biu.ac.il>
2021-03-08 18:07:04 +0200

    Initial commit

---
+++ app/src/main/java/com/fitastyclient/MainActivity.java
@@ -0,0 +1,15 @@
+package com.fitastyclient;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.fitastyclient.Utils;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Utils.log("hello");
+    }
+}
---
+++ app/src/main/java/com/fitastyclient/Utils.java
@@ -0,0 +1,9 @@
+package com.fitastyclient;
+
+import android.util.Log;
+
+public class Utils {
+    static public void log(String msg) {
+        Log.d("DEBUG", msg);
+    }
+}
